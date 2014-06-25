699b09ba2d2bde211f9ef67128b09d131db9b309
marc_comas <marc.comas@gmail.com>
2014-06-25 17:53:53 +0200

    First commit

---
+++ R/combining_partitions.R
@@ -0,0 +1,192 @@
+require(plyr)
+require(gridExtra)
+require(ggplot2)
+
+part_name = function(part) sprintf("(%s)", paste(sort(part), collapse=','))
+
+## PART B absorbes A. In this function part A is incorporated to B and after that partition A is eliminated
+b_absorbes_a = function(partition, partA, partB){
+  if(partA == partB){
+    stop( "Same part A and B")
+  }
+  if(! (partA %in% 1:length(partition) & partB %in% 1:length(partition)) ){
+    stop( "Some part out of range")
+  }
+  new_partition = partition
+  new_partition[[partB]] = c(new_partition[[partA]], new_partition[[partB]])
+  new_partition[[partA]] = NULL
+  names(new_partition) = laply(new_partition, part_name)
+  new_partition
+}
+
+#' Create a partition of classes from weights or probabilities
+#'
+#' This function applies the methodology described in [citar article]
+#' to build a hierarchy of classes using the weights or probabilities 
+#' that an element belongs to each class
+#' @param tau dataframe of probabilities/weights (\code{tau} must be strictly positive)
+#' @param varphi function with two parameters (\code{v_tau}, \code{a}). Parameter 
+#' \code{v_tau} is a vector of probabilities, parameter \code{a} is the a selected class.
+#' \code \code{varphi}(\code{v_tau}, \code{a}) gives the representativeness of element with
+#' probabities \code{v_tau} to class \code{a}.
+#' @param theta function with three parameters (\code{v_tau}, \code{a}, \code{b}).
+#' Parameter \code{v_tau} is a vector of probabilities, parameters \code{a} and \code{b}
+#' are classes to be combined.
+get_hierarchical_partition = function(tau, 
+                                      varphi, 
+                                      theta){
+  ctau = tau
+  K = ncol(ctau)
+  partitions = list()
+  partitions[[K]] = as.list(1:K)
+  names(partitions[[K]]) = laply(partitions[[K]], part_name)
+  for(k in K:2){
+    COMB = t(expand.grid(1:k, 1:k))
+    COMB = COMB[, COMB[1,] != COMB[2,]]
+    rownames(COMB) = c('a', 'b')
+    colnames(COMB) = col.names=apply(COMB, 2, paste, collapse='-')
+    to_merge = which.min( v <- aaply(COMB, 2, function(ind){
+      a = ind[1]; b = ind[2]
+      sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) * theta(v_tau, a, b) ) ) / sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) ) )
+    }) )
+    part = COMB[,to_merge]
+    partitions[[k-1]] = b_absorbes_a(partitions[[k]], part['a'], part['b'] )
+    ctau[,part['b']] = ctau[,part['a']] + ctau[,part['b']]
+    ctau  = ctau[,-part['a']]
+  }
+  class(partitions) = 'hpartition'
+  partitions
+}
+
+get_hierarchical_partition_mult_1 = function(tau, 
+                                      varphi = function( v_tau, a) if(which.max(v_tau) == a) 1 else 0, 
+                                      theta = function(v_tau, a, b) log(v_tau[a] / v_tau[b])^2){
+  ctau = tau
+  K = ncol(ctau)
+  partitions = list()
+  partitions[[K]] = as.list(1:K)
+  names(partitions[[K]]) = laply(partitions[[K]], part_name)
+  for(k in K:2){
+    COMB = t(expand.grid(1:k, 1:k))
+    COMB = COMB[, COMB[1,] != COMB[2,]]
+    rownames(COMB) = c('a', 'b')
+    colnames(COMB) = col.names=apply(COMB, 2, paste, collapse='-')
+    to_merge = which.min( v <- aaply(COMB, 2, function(ind){
+      a = ind[1]; b = ind[2]
+      sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) * theta(v_tau, a, b) ) ) / sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) ) )
+    }) )
+    part = COMB[,to_merge]
+    partitions[[k-1]] = b_absorbes_a(partitions[[k]], part['a'], part['b'] )
+    ctau[,part['b']] = sqrt(ctau[,part['a']] * ctau[,part['b']])
+    ctau  = ctau[,-part['a']]
+  }
+  class(partitions) = 'hpartition'
+  partitions
+}
+
+get_hierarchical_partition_mult_2 = function(tau, 
+                                             varphi,# = function( v_tau, a) if(which.max(v_tau) == a) 1 else 0, 
+                                             theta){# = function(v_tau, a, b) log(v_tau[a] / v_tau[b])^2){
+  ctau = tau
+  K = ncol(ctau)
+  partitions = list()
+  partitions[[K]] = as.list(1:K)
+  names(partitions[[K]]) = laply(partitions[[K]], part_name)
+  for(k in K:2){
+    COMB = t(expand.grid(1:k, 1:k))
+    COMB = COMB[, COMB[1,] != COMB[2,]]
+    rownames(COMB) = c('a', 'b')
+    colnames(COMB) = col.names=apply(COMB, 2, paste, collapse='-')
+    to_merge = which.min( v <- aaply(COMB, 2, function(ind){
+      a = ind[1]; b = ind[2]
+      sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) * theta(v_tau, a, b) ) ) / sum( apply(ctau, 1, function(v_tau) varphi(v_tau, a) ) )
+    }) )
+    part = COMB[,to_merge]
+    partitions[[k-1]] = b_absorbes_a(partitions[[k]], part['a'], part['b'] )
+    ctau = prop_partition_mult(tau, partitions[[k-1]])
+  }
+  class(partitions) = 'hpartition'
+  partitions
+}
+
+hp_entropy = function(tau){
+  varphi = function(v_tau, a) 1
+  theta = function(v_tau, a, b) -xlog(v_tau[a] + v_tau[b]) + xlog(v_tau[a]) + xlog(v_tau[b])
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_entropy.prop = function(tau){
+  varphi = function(v_tau, a) v_tau[a]
+  theta = function(v_tau, a, b) -xlog(v_tau[a] + v_tau[b]) + xlog(v_tau[a]) + xlog(v_tau[b])
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_entropy.dichotomic = function(tau){
+  varphi = function(v_tau, a) if(which.max(v_tau) == a) 1 else 0
+  theta = function(v_tau, a, b) -xlog(v_tau[a] + v_tau[b]) + xlog(v_tau[a]) + xlog(v_tau[b])
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_atchison.prop = function(tau){
+  varphi = function(v_tau, a) v_tau[a]
+  theta = function(v_tau, a, b) log(v_tau[a] / v_tau[b])^2
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_atchison.dichotomic = function(tau){
+  varphi = function(v_tau, a) if(which.max(v_tau) == a) 1 else 0
+  theta = function(v_tau, a, b) log(v_tau[a] / v_tau[b])^2
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_demp = function(tau){
+  varphi = function(v_tau, a) v_tau[a]
+  theta = function(v_tau, a, b) -if(which.max(v_tau) == b) 1 else 0
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_demp2 = function(tau){
+  varphi = function(v_tau, a) v_tau[a]
+  theta = function(v_tau, a, b) if(which.max(v_tau) != b) 1 else 0
+  get_hierarchical_partition(tau, varphi, theta)
+}
+hp_demp.prop = function(tau){
+  varphi = function(v_tau, a) v_tau[a]
+  theta = function(v_tau, a, b) -v_tau[b]
+  get_hierarchical_partition(tau, varphi, theta)
+}
+
+prop_partition = function(tau, partition)
+  do.call('cbind', llply(partition, function(part){
+    if(is.vector(tau[,part])) return(tau[,part])
+    apply(tau[,part], 1, sum)
+  }))
+
+prop_partition_mult = function(tau, partition)
+  do.call('cbind', llply(partition, function(part){
+    if(is.vector(tau[,part])) return(tau[,part])
+    apply(tau[,part], 1, prod)^(1/length(part))
+  }))
+
+cluster_partition = function(tau, partition)
+  names(partition)[apply( do.call('cbind', llply(partition, function(part){
+     if(is.vector(tau[,part])) return(tau[,part])
+     apply(tau[,part], 1, sum)
+     })), 1, which.max)]
+
+plot.hpartition = function(hp, tau, data, nrow = 2){
+  df = ldply(hp, function(partition){
+    df = data
+    df$cluster = cluster_partition(tau, partition)
+    df$step = factor(length(partition), levels =  length(hp):1)
+    df
+  })
+  
+  plots = llply(split(df, df$step), function(d) 
+    ggplot(data=d, aes(x=X1, y=X2, col=cluster)) + 
+      geom_point(size=2) + xlab("") + ylab("") + 
+      theme(legend.position="none"))#legend.title=element_blank()))
+  
+  do.call("grid.arrange", c(plots, nrow=nrow))
+}
+
+xlog <- function(x) {
+  xlog1d <- function (xi) if (xi == 0) 0 else (xi*log(xi))
+  
+  if (is.null(dim(x))) return(sapply(x,xlog1d))
+  else return(matrix(sapply(x,xlog1d),dim(x)))
+}
---
+++ R/mixture_function.R
@@ -0,0 +1,54 @@
+require(mvtnorm)
+# This function generates a mixture with specific
+mclust_rmixnorm = function(n, mclust_solution){
+  func_pi = mclust_solution$parameters$pro
+  func_mean = mclust_solution$parameters$mean
+  func_sigma = mclust_solution$parameters$variance$sigma
+  rmixnorm(n, 
+           func_pi,
+           func_mean,
+           func_sigma)
+}
+rmixmod_rmixnorm = function(n, mixmod_solution){
+  func_pi = mixmod_solution@bestResult@parameters@proportions
+  func_mean = t(mixmod_solution@bestResult@parameters@mean)
+  func_sigma = do.call('abind', list(mixmod_solution@bestResult@parameters@variance, along = 3))
+  rmixnorm(n, 
+           func_pi,
+           func_mean,
+           func_sigma)
+}
+
+rmixnorm = function(n, Pi, Mu, S, labels=F, ...){
+  z = sample(x=1:length(Pi), size=n, prob=Pi, replace=T)
+  rmn = matrix(0, nrow=n, ncol=nrow(Mu))
+  for(i in 1:length(Pi)){
+    n_z = sum(z==i)
+    if(n_z!=0){
+      if(ncol(Mu)==1)
+        rmn[z==i,] = rnorm(n_z, mean=Mu[,i], sd=S[,,i])
+      else
+        rmn[z==i,] = rmvnorm(n_z, mean=Mu[,i], sigma=S[,,i])
+    }
+  }
+  if(labels)
+    rmn = cbind(rmn, z)
+  rmn
+}
+
+get_order = function(fittedMean, fittedVariance, mainMean, mainVariance){
+  K = ncol(mainMean)
+  res = matrix(0, nrow=K, ncol=K)
+  for(i in 1:K){
+    for(j in 1:K){
+      res[i,j] = norm(mainVariance[,,j]-fittedVariance[,,i], type="2") + norm(mainMean[,j]-fittedMean[,i], type = "2")
+    }
+  }
+  return( apply(res, 1, which.min))
+}
+
+clr_mixnorm = function(X, Pi, Mu, Sigma){
+  log.dnorm = llply( 1:length(Pi), function(i) log(Pi[i]) + dmvnorm(X, mean=Mu[,i], sigma=Sigma[,,i], log=T))
+  log.dnorm.mean = Reduce('+', log.dnorm) / length(log.dnorm)
+  data.frame(do.call(cbind, llply( log.dnorm, function(comp) comp - log.dnorm.mean)))
+}
