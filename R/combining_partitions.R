cf3e3fec0733a733bbc9a417bd4dc6eec5cb0fa4
marc_comas <marc.comas@gmail.com>
2015-06-03 12:33:10 +0200

    Documentation included

--- R/combining_partitions.R
+++ R/combining_partitions.R
@@ -144,7 +144,23 @@ prop_partition_mult = function(tau, partition)
     apply(tau[,part], 1, prod)^(1/length(part))
   }))
 
-#'
+#' Build a hierchical partition from posterior probabilities
+#' 
+#' This function applies the methodology described in [citar article]
+#' to build a hierarchy of classes using the weights or probabilities 
+#' that an element belongs to each class
+#' @param tau dataframe of probabilities/weights (\code{tau} must be strictly positive)
+#' 
+#' @param omega function with two parameters (\code{v_tau}, \code{a}). Parameter 
+#' \code{v_tau} is a vector of probabilities, parameter \code{a} is the a selected class.
+#' \code{omega}(\code{v_tau}, \code{a}) gives the representativeness of element with
+#' probabities \code{v_tau} to class \code{a}
+#' 
+#' @param lambda function with three parameters (\code{v_tau}, \code{a}, \code{b}).
+#' Parameter \code{v_tau} is a vector of probabilities, parameters \code{a} and \code{b}
+#' are classes to be combined.
+#' @param func defines the way components are merged
+#' 
 #' @export
 get_hierarchical_partition_mult_func = function(tau, 
                                                 varphi,# = function( v_tau, a) if(which.max(v_tau) == a) 1 else 0, 
