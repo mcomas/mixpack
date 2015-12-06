617d65ba25a35e132865dd4af2fcd74ff733d062
mcomas <marc@macmini.local>
2015-12-06 12:15:36 +0100

    0.3.0 Version submitted to CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,4 @@
 #formatR::tidy_dir("R", indent = getOption("formatR.indent", 2), arrow = getOption("formatR.arrow", T))
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to mixpack. mixpack is a package for mixture component visualization")
-  
+  packageStartupMessage("mixpack: a package for mixture components analysis")
 }
